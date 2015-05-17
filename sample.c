8b18b0453b8ddf576acf25e6dbc3b52367ab1c6e
Ubuntu <ubuntu@ip-172-31-21-88.us-west-2.compute.internal>
2015-05-17 04:04:56 +0000

    first C program

---
+++ sample.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#define N 16
+
+int main(void) {
+  int n;           /* The current exponent */
+  int val = 1;     /* The current power of 2  */
+
+  printf("\t  n  \t    2^n\n");
+  printf("\t================\n");
+  for (n=0; n<=N; n++) {
+    printf("\t%3d \t %6d\n", n, val); 
+    val = 2*val;
+  }
+  return 0;
+}
+
+
